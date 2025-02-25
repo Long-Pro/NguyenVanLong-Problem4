813afbacbecf6bc3c9f84f6e7c4ab5612ee04767
nuyo <nuyo@nexondv.com>
2025-02-25 10:22:16 +0700

    resolve problem 4: Three ways to sum to n

---
+++ index.ts
@@ -0,0 +1,21 @@
+function sum_to_n_a(n: number): number {
+  // algorithm complexity: O(1)
+	return (1+n)*n/2
+}
+
+function sum_to_n_b(n: number): number {
+  // algorithm complexity: O(n)
+	let result=1
+  for(let i=2;i<=n;i++) result+=i
+  return result
+}
+
+function sum_to_n_c(n: number): number {
+  // algorithm complexity: O(n)
+  let result = 0;
+  while (n) {
+    console.log({ n });
+    result += n--;
+  }
+  return result;
+}
